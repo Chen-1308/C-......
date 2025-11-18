e67a2a2895d963160521b74adeb69c9466c44e2d
Chen-1308 <2582043701@qq.com>
2025-11-18 22:50:27 +0800

    Add files via upload

--- els game/SOURCE/源.cpp
+++ els game/SOURCE/源.cpp
@@ -6,6 +6,9 @@ int nFieldWidth = 12;
 int nFieldHeight = 18;
 unsigned char* pField = nullptr;
 
+int nScreenWidth = 80;            //Console Screen Size X (columns
+int nScreenHeight = 30;           //Console Screen Size Y (rows)
+
 int Rotate(int px, int py, int r)
 {
 	switch (r % 4)
@@ -17,6 +20,17 @@ int Rotate(int px, int py, int r)
 	}
 	return 0;
 } 
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			//Get index into piece
+			int pi = Rotate(px, py, nRotation);
+		}
+	return true;
+}
 
 int main()
 {
@@ -57,7 +71,41 @@ int main()
 	tetromino[6].append(L".XX.");
 
 	pField = new unsigned char[nFieldWidth * nFieldHeight];//Create play field buff
-	0; x < nFieldWidth; x++) //Board Boundary
-	y = 0; y < nFieldHeight; y++)
-	Ld[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+	for (int x = 0; x < nFieldWidth; x++) //Board Boundary
+		for (int y = 0; y < nFieldHeight; y++)
+			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';  
+	HANDLE hConsole = CreatConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);      
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	bool bGameOver = false;
+
+	while (!bGameOver)
+	{
+		//GAME TIMING =================================================
+		
+
+		// INOUT ======================================================
+		
+
+
+		// GAME LOGIC =================================================
+		
+
+
+		// RENDER OUTPUT ==============================================
+		
+		
+		//Draw Field
+		for (int x = 0; x < nFieldWidth; x++)
+			for (int y = 0; y < nFieldHeight; y++)
+				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+		// Display Frame
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+	}
+	return 0;
 }
+
